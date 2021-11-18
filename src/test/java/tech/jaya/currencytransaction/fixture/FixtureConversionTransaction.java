2905f65253b7d1bb4776a1d1a328ae3ddf61a7e1
Danilo da Silva Pereira <t0305dnl@prestadorcbmp.com.br>
2021-11-18 17:44:49 -0300

    [unit-test]: Write unit test for usecase and message component

--- src/test/java/tech/jaya/currencytransaction/fixture/FixtureConversionTransaction.java
+++ src/test/java/tech/jaya/currencytransaction/fixture/FixtureConversionTransaction.java
@@ -3,11 +3,12 @@ package tech.jaya.currencytransaction.fixture;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import tech.jaya.currencytransaction.core.model.ConversionTransaction;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -49,5 +50,47 @@ public class FixtureConversionTransaction {
                 .conversionRate(new BigDecimal("1.134"))
                 .transactionTime(LocalDateTime.now())
                 .build();
+    }
+
+    public static ConversionTransaction oneWithoutSave(final String userIdentifier) {
+        return ConversionTransaction.builder()
+                .userIdentifier(userIdentifier)
+                .originCurrency("EUR")
+                .destinationCurrency("USD")
+                .originValue(new BigDecimal("20"))
+                .build();
+    }
+
+    public static ConversionTransaction oneWithSameCurrency(final String userIdentifier) {
+        return ConversionTransaction.builder()
+                .userIdentifier(userIdentifier)
+                .originCurrency("BRL")
+                .destinationCurrency("brl")
+                .originValue(new BigDecimal("20"))
+                .build();
+    }
+
+    public static List<ConversionTransaction> list(final String userIdentifier) {
+        return Arrays.asList(ConversionTransaction.builder()
+                        .identifier(UUID.randomUUID().toString())
+                        .userIdentifier(userIdentifier)
+                        .originCurrency("EUR")
+                        .destinationCurrency("USD")
+                        .originValue(new BigDecimal("20"))
+                        .destinationValue(new BigDecimal("22.68"))
+                        .conversionRate(new BigDecimal("1.134"))
+                        .transactionTime(LocalDateTime.now())
+                        .build(),
+                ConversionTransaction.builder()
+                        .identifier(UUID.randomUUID().toString())
+                        .userIdentifier(userIdentifier)
+                        .originCurrency("JPY")
+                        .destinationCurrency("GBP")
+                        .originValue(new BigDecimal("20"))
+                        .destinationValue(new BigDecimal("22.68"))
+                        .conversionRate(new BigDecimal("1.134"))
+                        .transactionTime(LocalDateTime.now())
+                        .build()
+        );
     }
 }
